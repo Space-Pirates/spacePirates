745c1f8d03b58b2d9d77f3c1797128ad178bcd1b
nassir <nassirmz@gmail.com>
2016-04-28 15:03:29 -0700

    (feat) Add index.js file for server listen

--- server/index.js
+++ server/index.js
@@ -4,3 +4,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log('Server listening ', port);
 });
+
--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 var express = require('express');
-<<<<<<< 83e74281ddc6c50b662a7ce84ca673505576e6e6
 var middleware = require('./utils/middleware');
 
 var app = express();
 middleware(app, express);
 
 module.exports = app;
-=======
+
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -14,6 +13,3 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 module.exports = app;
 
-
-
->>>>>>> (feat) Add basic server to serve client folder
