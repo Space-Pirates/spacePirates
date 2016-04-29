ae758044f22e7137ee095b517faa955104c24e8a
Jak Buttarazzi <JakRazzi@gmail.com>
2016-04-28 21:30:31 -0500

    (refactor) Clean up code and abstract middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var middleware = require('./utils/middleware');
 
 var app = express();
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../client'));
+middleware(app, express);
+
 module.exports = app;
-
-
-
