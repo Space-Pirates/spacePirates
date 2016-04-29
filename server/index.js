c4c15d0cf70e17afa521d446f1d1b2f1ced68267
Rob Bruhn <rdbruhn@gmail.com>
2016-04-29 18:46:50 -0500

    (feat) Add route to serve index.html

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 var app = require('./server');
+
 var port = process.env.PORT || 5000;
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 app.listen(port, function () {
   console.log('Server listening ', port);
