b410984ad559b3a5447524a054ad77411b9f8446
Jak Buttarazzi <jakrazzi@gmail.com>
2016-04-30 17:27:59 -0500

    (test) Integrate database with deck class tests (#41)
    * (refactor) Clean up deck specs
    
    * (fix) Use correct relationship arguments
    
    * (test) Create test document in db before each test
    
    * (test) Connect setTiles tests to database
    
    * (refactor) Handle errors in getTiles test
    
    * (refactor) Use unique values to test shuffle method
    
    * (test) hook up database to dealTile tests
    
    * (test) Hook database up to Deck initialize tests

--- server/game/game-specs/deck-spec.js
+++ server/game/game-specs/deck-spec.js
@@ -1,16 +1,63 @@
 var chai = require('chai');
 var Deck = require('../deck');
 var tileDictionary = require('../tile-dictionary.json');
+var db = require('../../db/db');
 var expect = chai.expect();
-var collection, deck;
+var collection, deck, doc;
+
+function makePlayer(cb, done) {
+  var playerId;
+
+  new db.Player({
+    gameId: 'testGameId',
+    role: 'pirate',
+    isTurn: false,
+    hand: [],
+    debuffs: []
+  })
+  .save()
+  .then(function(data) {
+    playerId = data;
+    done();
+  })
+  .catch(function(err) {
+    console.error(err);
+    done();
+  });
+
+  cb(playerId);
+}
 
 module.exports = function() {
 
-  beforeEach(function (){
-    deck = new Deck();
+  beforeEach(function (done){
+    deck = new Deck('testGameId');
+    new db.Deck({
+      gameId: 'testGameId',
+      lastDiscard: '',
+      tilesRemaining: 54,
+      tiles: []
+    })
+    .save()
+    .then(function() {
+      done();
+    })
+    .catch(function(err) {
+      console.error(err);
+      done();
+    });
   });
-  afterEach(function() {
-    deck = undefined;
+
+  afterEach(function (done) {
+    db.Deck.filter({gameId: 'testGameId'})
+    .delete()
+    .then(function() {
+      done();
+    })
+    .catch(function(err) {
+      console.error(err);
+      done();
+    });
   });
 
   it('should exist', function() {
@@ -19,16 +66,34 @@ module.exports = function() {
   it('should be a class with psuedoclassical instantiation', function() {
     expect(deck).to.be.an('Object');
   });
-  it('should have a gameID property', function() {
-    expect(deck.gameID).to.be.a('number');
+  it('should have a gameId property', function() {
+    expect(deck.gameID).to.equal('testGameId');
   });
 
   describe('setTiles method', function () {
     it('should exist', function() {
       expect(deck.setTiles).to.be.a('function');
     });
-    // TODO: Hook tests up to database
-    it('should set the collection of tiles in the database');
+    it('should set the collection of tiles in the database', function(done) {
+      deck.setTiles(['a', 'b', 'c'])
+      .then(function() {
+        db.Deck.filter({gameId: 'testGameId'}).run()
+        .then(function(data) {
+          doc = data;
+          done();
+        })
+        .catch(function(err) {
+          console.error(err);
+          done();
+        });
+      })
+      .catch(function(err) {
+        console.error(err);
+        done();
+      });
+
+      expect(doc.tiles).to.deep.equal(['a', 'b', 'c']);
+    });
   });
 
   describe('getTiles method', function () {
@@ -36,18 +101,25 @@ module.exports = function() {
       expect(deck.getTiles).to.be.a('function');
     });
     it('should return a collection of tile objects', function(done) {
-      deck.setTiles(['a', 'b', 'c'])
+      deck.setTiles(['d', 'e', 'f'])
       .then(function() {
         deck.getTiles()
-        .then(function(collection) {
-          collection = deck.getTiles();
+        .then(function(data) {
+          collection = data;
+          done();
+        })
+        .catch(function(err) {
+          console.error(err);
           done();
         });
+      })
+      .catch(function(err) {
+        console.error(err);
+        done();
       });
 
       expect(collection).to.be.an('array');
-      expect(collection).to.deep.equal(['a', 'b', 'c']);
-      collection = undefined;
+      expect(collection).to.deep.equal(['d', 'e', 'f']);
     });
   });
 
@@ -56,23 +128,41 @@ module.exports = function() {
       expect(deck.shuffle).to.be.a('function');
     });
     it('should randomize the collection of tiles', function() {
-      collection = ['a', 'b', 'c'];
+      collection = ['g', 'h', 'i'];
       var shuffled = deck.shuffle(collection);
 
       expect(shuffled).to.not.deep.equal(collection);
       expect(shuffled).to.have.length(collection.length);
-      expect(shuffled).to.contain('a');
-      expect(shuffled).to.contain('b');
-      expect(shuffled).to.contain('c');
-      collection = undefined;
+      expect(shuffled).to.contain('g');
+      expect(shuffled).to.contain('h');
+      expect(shuffled).to.contain('i');
     });
   });
 
   describe('dealTile method', function () {
+    var playerId;
 
     beforeEach(function (done) {
       deck.setTiles(['a', 'b', 'c'])
       .then(function() {
+        makePlayer(function(data) {
+          playerId = data.id;
+        }, done);
+      })
+      .catch(function(err) {
+        console.error(err);
+        done();
+      });
+    });
+
+    afterEach(function (done) {
+      db.Player
+      .delete()
+      .then(function() {
+        done();
+      })
+      .catch(function(err) {
+        console.error(err);
         done();
       });
     });
@@ -80,35 +170,46 @@ module.exports = function() {
     it('should exist', function() {
       expect(deck.dealTile).to.be.a('function');
     });
-    // TODO: This needs to be refactored for database interaction
-    it('should give a player a tile', function() {
-      var player = {
-        hand: []
-      };
-
-      deck.dealTile(player);
+    it('should give a player a tile', function(done) {
+      var player;
+      deck.dealTile(playerId)
+      .then(function() {
+        db.Player.get(playerId).run()
+        .then(function(data) {
+          player = data;
+          done();
+        })
+        .catch(function(err) {
+          console.error(err);
+          done();
+        });
+      })
+      .catch(function(err) {
+        console.error(err);
+        done();
+      });
       expect(player.hand).to.deep.equal(['c']);
-      deck.dealTile(player);
-      deck.dealTile(player);
-      expect(player.hand).to.deep.equal(['c', 'b', 'a']);
     });
-    // TODO: This needs to be refactored for database interaction
-    it('should remove the given tile from the deck', function() {
-      var player = {
-        hand: []
-      };
+    it('should remove the given tile from the deck', function(done) {
+      var tiles;
+      deck.dealTile(playerId)
+      .then(function() {
+        deck.getTiles()
+        .then(function(data) {
+          tiles = data;
+          done();
+        })
+        .catch(function(err) {
+          console.error(err);
+          done();
+        });
+      })
+      .catch(function(err) {
+        console.error(err);
+        done();
+      });
 
-      deck.dealTile(player);
-      deck.getTiles()
-      .then(function(tiles) {
-        expect(tiles).to.deep.equal(['a', 'b']);
-      });
-      deck.dealTile(player);
-      deck.dealTile(player);
-      deck.getTiles()
-      .then(function(tiles) {
-        expect(tiles).to.have.length(0);
-      });
+      expect(tiles).to.deep.equal(['a', 'b']);
     });
   });
 
@@ -125,10 +226,6 @@ module.exports = function() {
       });
     });
 
-    afterEach(function () {
-      collection = undefined;
-    });
-
     it('should exist', function() {
       expect(deck.initialize).to.be.a('function');
     });
@@ -140,7 +237,24 @@ module.exports = function() {
       expect(collection.slice(0, 9)).to.not.deep.equal(tileDictionary.slice(0, 9));
     });
     // TODO: Hook this up to database
-    it('should deal 3 tiles to each player');
+    it('should deal 3 tiles to each player', function(done) {
+      var players = [];
+      makePlayer(function(player) {
+        players.push(player);
+      }, function() {
+        makePlayer(function(player) {
+          players.push(player);
+        }, function() {
+          makePlayer(function(player) {
+            players.push(player);
+          }, done);
+        });
+      });
+
+      expect(players[0].hand.length).to.equal(3);
+      expect(players[1].hand.length).to.equal(3);
+      expect(players[2].hand.length).to.equal(3);
+    });
     it('should have 54 tiles remaining in the collection', function() {
       expect(collection).to.have.length(54);
     });
