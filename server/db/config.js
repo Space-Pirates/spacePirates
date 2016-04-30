8278a6b2615923ee42716d24915c9a9a9e8c3289
Mike Khanoyan <mherkhanoyan@gmail.com>
2016-04-29 19:21:38 -0700

    (refactor) Refactor to using the thinky ORM

--- server/db/config.js
+++ server/db/config.js
@@ -2,9 +2,5 @@ module.exports = {
   host: process.env.RDB_HOST|| 'localhost',
   port: process.env.RDB_PORT || 28015,
   db: process.env.RDB_DB || 'space_pirates',
-  db: 'space_pirates',
-  tables: {
-    'users': 'users_id',
-    'games': 'games_id'
-  }
+  authKey: ''
 };
--- server/db/main.js
+++ server/db/main.js
@@ -1,41 +1,5 @@
-var r = require('rethinkdb');
 var dbConfig = require('./config');
+var thinky = require('thinky')(dbConfig);
 
-module.exports.connect = function(cb) {
-  r.connect(dbConfig.host, function (err, connection) {
-    if (err) {
-      throw err;
-    }
-    connection['_id'] = Math.floor(Math.random()*10001);
-    cb(connection);
-  });
-};
+module.exports = thinky;
 
-module.exports.setup = function() {
-  r.connect({host: dbConfig.host, port: dbConfig.port }, function (err, connection) {
-    if (err) {
-      throw err;
-    }
-    r.dbCreate(dbConfig.db).run(connection, function(err, result) {
-      if(err) {
-        console.log("[DEBUG] RethinkDB database '%s' already exists (%s:%s)\n%s", dbConfig.db, err.name, err.msg, err.message);
-      }
-      else {
-        console.log("[INFO ] RethinkDB database '%s' created", dbConfig.db);
-      }
-
-      for(var tbl in dbConfig.tables) {
-        (function (tableName) {
-          r.db(dbConfig.db).tableCreate(tableName).run(connection, function(err, result) {
-            if(err) {
-              console.log("[DEBUG] RethinkDB table '%s' already exists (%s:%s)\n%s", tableName, err.name, err.msg, err.message);
-            }
-            else {
-              console.log("[INFO ] RethinkDB table '%s' created", tableName);
-            }
-          });
-        })(tbl);
-      }
-    });
-  });
-};
