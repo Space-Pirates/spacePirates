8819dc564ac3576dbee545eceb887c9b0a97b75b
Rob Bruhn <rdbruhn@gmail.com>
2016-04-30 13:38:15 -0500

    (feat) Add material scripts and structure to menu *Build mock data for games in menu controller

--- client/main-app/app.js
+++ client/main-app/app.js
@@ -1,4 +1,7 @@
 angular.module('spacePirates', [
+  'ngMaterial',
+  'ngMessages', 
+  'material.svgAssetsCache',
   'ui.router',
   'app.menu',
   'app.game',
--- client/main-app/menu/menu.js
+++ client/main-app/menu/menu.js
@@ -7,6 +7,10 @@ angular.module('app.menu', [])
         controller: 'MenuController'
       });
   }])
-  .controller('MenuController', ['$scope', function($scope){
-    $scope.data = {};
+  .controller('MenuController', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+    //Mock room data
+    $scope.rooms = [{id:1, players:['Rob','Jak','Mike','Nassir']},{id:2, players:['Rob','Jak','Mike','Nassir']},{id:3, players:['Rob','Jak']},{id:4, players:['Mike','Nassir']},{id:5, players:['Nassir']},]
+    $scope.createGame = function () {
+      //call to backEnd to create new gameState
+    };
   }]);
