fa6e9626248ba0e88b5a4dc4ec901903d00270bb
Jak Buttarazzi <JakRazzi@gmail.com>
2016-04-30 13:04:53 -0500

    (refactor) Configure relationships for game documents

--- server/db/models/game.js
+++ server/db/models/game.js
@@ -1,14 +1,21 @@
 var thinky = require('./../thinky');
 var User = require('./user');
+var Board = require('./board');
+var Deck = require('./deck');
+var Player = require('./player');
 
 var type = thinky.type;
 
 var Game = thinky.createModel('Game', {
   id: type.string(),
   // ownerId: type.string(),
-  deckId: type.string(),
-  boardId: type.string()
+  boardId: type.string(),
+  deckId: type.string()
 });
+
+Game.hasOne(Board, 'board', 'boardId', 'id');
+Game.hasOne(Deck, 'deck', 'deckId', 'id')
+Game.hasMany(Player, 'players', 'id', 'gameId');
 
 Game.hasAndBelongsToMany(User, 'id', 'id');
 
