592ef044a2b719af1ed5d5022fc01c32a1ec70e2
Nassir Zeinu <nassirmz@gmail.com>
2016-05-02 16:28:12 -0700

    (feat) Add auth0 (#45)
    * (feat) Add basic server to serve client folder
    
    * (feat) Add index.js file for server listen
    
    * (feat) Implement database connection
    
    * (feat) Add script tags for auth0 libraries
    
    * (feat) Add controller for auth0
    
    * (refactor) Refactor front-end routing
    
    * (chor) Add dependecies to bower and npm
    
    * (feat) Add auth0 view
    
    * (refactor) Use bower auth0 dependencies instead of cdn

--- server/server.js
+++ server/server.js
@@ -5,3 +5,11 @@ var app = express();
 middleware(app, express);
 
 module.exports = app;
+
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client'));
+module.exports = app;
+
