50b9344800b47e79eddff4f9c50e849cfdfa55b5
nassir <nassirmz@gmail.com>
2016-05-02 15:59:00 -0700

    (refactor) Refactor front-end routing

--- client/main-app/menu/menu.js
+++ client/main-app/menu/menu.js
@@ -1,12 +1,4 @@
 angular.module('app.menu', [])
-  .config(['$stateProvider', function($stateProvider){
-    $stateProvider
-      .state('menu', {
-        url:'',
-        templateUrl: '/main-app/menu/menu.html',
-        controller: 'MenuController'
-      });
-  }])
   .controller('MenuController', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
     //Mock room data
     $scope.rooms = [{id:1, players:['Rob','Jak','Mike','Nassir']},{id:2, players:['Rob','Jak','Mike','Nassir']},{id:3, players:['Rob','Jak']},{id:4, players:['Mike','Nassir']},{id:5, players:['Nassir']},]
