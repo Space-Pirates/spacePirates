00f1bcd2bf06b59fd7d15db15747fc14aecc10a8
nassir <nassirmz@gmail.com>
2016-05-03 15:44:52 -0700

    (feat) Implement password hashing

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,5 +1,5 @@
 var thinky = require('./../thinky');
-
+var bcrypt = require('bcrypt-nodejs');
 var type = thinky.type;
 
 var User = thinky.createModel('User', {
@@ -11,6 +11,36 @@ var User = thinky.createModel('User', {
   password: type.string()
 });
 
+User.pre('save', function (next) {
+  var user = this;
+
+  // generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+   }
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        next(err);
+      }
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+User.define('comparePassword', function (attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+     if (err) {
+       callback(err);
+     }
+     callback(null, isMatch);
+   });
+});
+
+
 module.exports = User;
 
 // Must come after export
@@ -18,3 +48,4 @@ var Game = require('./game');
 
 User.hasAndBelongsToMany(Game, 'games', 'id', 'id');
 // User.hasMany(Game, 'ownedGames', 'id', 'ownerId');
+
