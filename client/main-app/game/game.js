19086ae8e441b40bc49a279209f7457d82b49925
Rob Bruhn <rdbruhn@gmail.com>
2016-05-03 17:27:45 -0500

    (chore) Remove stateProvider and move to main.js

--- client/main-app/game/game.js
+++ client/main-app/game/game.js
@@ -1,19 +1,4 @@
 angular.module('app.game', [])
-.config(['$stateProvider', function($stateProvider) {
-  $stateProvider
-    .state('game', {
-      url: '/game',
-      abstract: true,
-      template: '<div class="game">\
-                  <div ui-view></div>\
-                </div>'
-    })
-    .state('game.play', {
-      url: '/:id',
-      template: '<game-canvas></game-canvas>',
-      controller: 'GameController'
-    });
-}])
 .controller('GameController', ['$scope', function($scope){
 
 }])
