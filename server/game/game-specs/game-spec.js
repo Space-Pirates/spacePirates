7d3051689fa2d65aa01b6f658f2ced456e1324f3
Jak Buttarazzi <JakRazzi@gmail.com>
2016-05-04 14:31:36 -0500

    (test) Write tests for initialize method on Game class

--- server/game/game-specs/game-spec.js
+++ server/game/game-specs/game-spec.js
@@ -1,4 +1,6 @@
 var Game = require('../game');
+var db = require('../../db/db');
+var _ = require('underscore');
 
 describe('properties', function () {
   
@@ -23,9 +25,40 @@ describe('properties', function () {
 });
 
 describe('initialize method', function () {
-  it('should create a new create a new game document in the database');
-  it('should keep reference to the gameId');
-  it('should retain access to a new board instance');
-  it('should retain access to a new deck instance');
-  it('should retain access to all player instances');
+  var game = new Game();
+  var doc;
+
+  before(function (done) {
+    db.Game.get(game.gameId)
+    .run()
+    .then(function(data) {
+      doc = data;
+      done();
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
+
+  it('should create a new create a new game document in the database', function(done) {
+    expect(doc).to.be.an('object');
+    expect(doc.id).to.be.a('string');
+  });
+  it('should keep reference to the gameId', function() {
+    expect(game.gameId).to.equal(doc.id);
+  });
+  it('should retain access to a new board instance', function() {
+    expect(game.board).to.be.an('object');
+    expect(game.board).to.have.keys('gameId');
+    expect(game.board.gameId).to.equal(game.gameId);
+  });
+  it('should retain access to a new deck instance', function() {
+    expect(game.deck).to.be.an('object');
+    expect(game.deck).to.have.keys('gameId');
+    expect(game.deck.gameId).to.equal(game.gameId);
+  });
+  it('should retain access to all player instances', function() {
+    expect(game.players).to.be.an('object');
+    expect(_.size(game.players)).to.equal(4);
+  });
 });
