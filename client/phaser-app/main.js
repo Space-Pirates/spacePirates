8ec88020bf8fb44a08af04350161a74aafcd85f4
Rob Bruhn <rdbruhn@gmail.com>
2016-05-03 19:34:02 -0500

    (fix) Change start state to load and add hand and discard vars

--- client/phaser-app/main.js
+++ client/phaser-app/main.js
@@ -1,9 +1,8 @@
 window.createGame = function(ele, scope, players, mapId, injector) {
   window.game = new Phaser.Game(840, 550, Phaser.AUTO, 'gameCanvas');
-   window.dragPosition = new Phaser.Point(0,0);
-  var player1, player2, player3, player4, dropZone;
-  var x, y;
+  window.dragPosition = new Phaser.Point(0,0);
+  var player1, player2, player3, player4, dropZone, x, y, discard, hand;
   game.state.add('load', loadState);
   game.state.add('game', gameState);
-  game.state.start('game');
+  game.state.start('load');
 };
