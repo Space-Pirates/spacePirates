005485499c6b8fcc4182df52b775ecd56091bfb4
Jak Buttarazzi <JakRazzi@gmail.com>
2016-05-04 13:42:05 -0500

    (test) Create framework of tests for backend Game class

--- server/game/game-specs/game-spec-runner.js
+++ server/game/game-specs/game-spec-runner.js
@@ -1,8 +1,10 @@
+var gameSpecs = require('./game-spec');
 var deckSpecs = require('./deck-spec');
 var boardSpecs = require('./board-spec');
 var playerSpecs = require('./player-spec');
 
 describe('Backend Game Logic', function () {
+  describe('Game Class', gameSpecs);
   describe('Deck Class', deckSpecs);
   describe('Board Class', boardSpecs);
   describe('Player Class', playerSpecs);
---
+++ server/game/game-specs/game-spec.js
@@ -0,0 +1,14 @@
+describe('properties', function () {
+  it('should have a gameID');
+  it('should have a board object');
+  it('should have a deck object');
+  it('should have a players object');
+});
+
+describe('initialize method', function () {
+  it('should create a new create a new game document in the database');
+  it('should keep reference to the gameId');
+  it('should retain access to a new board instance');
+  it('should retain access to a new deck instance');
+  it('should retain access to all player instances');
+});
