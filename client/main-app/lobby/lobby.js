994da83330d66798f63bd5cf5d46a05ffb0a6a15
Rob Bruhn <rdbruhn@gmail.com>
2016-05-05 13:31:08 -0500

    (feat) Build Lobby factory and integrate into controller (#63)
    * (feat) Adjust lobby html to call createGame and joinGame
    
    * (feat) Remove mock data, add LobbyFactory, getGames, createGame, and joinGame
    
    * (feat) Create lobbyFactory file and module
    
    * (feat) Add store to controller and pass user to createGame
    
    * (feat) Implement getGames and createGame http factory methods
    
    * (style) Add new lines
    
    * (fix) change state string to include variable

--- client/main-app/lobby/lobby.js
+++ client/main-app/lobby/lobby.js
@@ -1,8 +1,26 @@
 angular.module('app.lobby', [])
-  .controller('LobbyController', ['$scope', function($scope){
+  .controller('LobbyController', ['$scope', 'LobbyFactory', '$state', 'store', function($scope, LobbyFactory, $state, store){
     //Mock room data
-    $scope.rooms = [{id:1, players:['Rob','Jak','Mike','Nassir']},{id:2, players:['Rob','Jak','Mike','Nassir']},{id:3, players:['Rob','Jak']},{id:4, players:['Mike','Nassir']},{id:5, players:['Nassir']},]
+    $scope.rooms = [];
+    var user = JSON.parse(store.get('com.spacePirates'));
+    $scope.getGames = function(){
+      LobbyFactory.getGames()
+        .then(function(games){
+          $scope.rooms = games;
+        });
+    };
+
     $scope.createGame = function () {
       //call to backEnd to create new gameState
+      LobbyFactory.createGame(user)
+        .then(function(gameId){
+          $scope.joinGame(gameId);
+        });
     };
+    
+    $scope.joinGame = function (gameId){
+      $state.go('game.play({id:' + gameId +'})');
+    };
+
+    $scope.getGames();
   }]);
