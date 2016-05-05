1584bc8faaddb86ca6f55da2947ab942c7ed8ea8
Rob Bruhn <rdbruhn@gmail.com>
2016-05-05 13:17:09 -0500

    (feat) Implement getGames and createGame http factory methods

--- client/main-app/lobby/lobbyFactory.js
+++ client/main-app/lobby/lobbyFactory.js
@@ -1,4 +1,31 @@
 angular.module('app.lobby',[])
 .factory('LobbyFactory', ['$http', function($http){
+  var getGames = function(){
+    $http({
+      method: 'GET',
+      url: '/game/'
+    }).then(function successCallback(resp){
+        return resp.data;
+    }, function errorCallback(resp){
+        console.log(resp.data);
+    });
+  };
+
+  var createGame = function(creator){
+     $http({
+      method: 'POST',
+      url: '/game/',
+      data: creator
+    }).then(function successCallback(resp){
+        return resp.data;
+    }, function errorCallback(resp){
+        console.log(resp.data);
+    });
+  };
+
+  return{
+    getGames: getGames,
+    createGame: createGame
+  };
 
 }]);
