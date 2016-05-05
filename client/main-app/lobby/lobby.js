af91f3ab4f4e3fe6b79079d771664486e7f561fb
Rob Bruhn <rdbruhn@gmail.com>
2016-05-05 13:04:13 -0500

    (feat) Remove mock data, add LobbyFactory, getGames, createGame, and joinGame

--- client/main-app/lobby/lobby.js
+++ client/main-app/lobby/lobby.js
@@ -1,8 +1,26 @@
 angular.module('app.lobby', [])
-  .controller('LobbyController', ['$scope', function($scope){
+  .controller('LobbyController', ['$scope', 'LobbyFactory', '$state', function($scope, LobbyFactory, $state){
     //Mock room data
-    $scope.rooms = [{id:1, players:['Rob','Jak','Mike','Nassir']},{id:2, players:['Rob','Jak','Mike','Nassir']},{id:3, players:['Rob','Jak']},{id:4, players:['Mike','Nassir']},{id:5, players:['Nassir']},]
+    $scope.rooms = [];
+    
+    $scope.getGames = function(){
+      LobbyFactory.getGames()
+        .then(function(games){
+          $scope.rooms = games;
+        });
+    };
+
     $scope.createGame = function () {
       //call to backEnd to create new gameState
+      LobbyFactory.createGame()
+        .then(function(gameId){
+          $scope.joinGame(gameId);
+        });
     };
+    
+    $scope.joinGame = function (gameId){
+      $state.go('game.play({id: gameId})');
+    };
+
+    $scope.getGames();
   }]);
