20bdcddf7d0985604b039fe55ef5245a1dbb6694
Rob Bruhn <rdbruhn@gmail.com>
2016-05-07 18:25:48 -0500

    (fix) Remove mock data, assign returned game data to scope.games

--- client/main-app/lobby/lobby.js
+++ client/main-app/lobby/lobby.js
@@ -1,12 +1,12 @@
 angular.module('app.lobby', ['app.lobbyFact'])
   .controller('LobbyController', ['$scope', 'LobbyFactory', '$state', 'store', function($scope, LobbyFactory, $state, store){
     //Mock room data
-    $scope.games = [{id: '555342342', players: ['Rob','Jak']}];
+    $scope.games = [];
     var user = JSON.parse(store.get('com.spacePirates'));
     $scope.getGames = function(){
       LobbyFactory.getGames()
         .then(function(games){
-         // $scope.games = games;
+         $scope.games = games;
         });
     };
 
