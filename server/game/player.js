471c95ffe884f5cde8cd38191baaf5ba29408614
Jak Buttarazzi <JakRazzi@gmail.com>
2016-05-07 19:12:23 -0500

    (feat) Add setRole and changeTurn methods to Player class

--- server/game/player.js
+++ server/game/player.js
@@ -1,32 +1,30 @@
 var db = require('../db/db');
 
-var Player = function (gameID, socketID) {
-  this.gameID = gameID;
-  this.socketID = socketID;
+var Player = function (gameId, socketId) {
+  this.gameId = gameId;
+  this.socketId = socketId;
   this.lastPlayed = {};
-  this.playerID = '';
+  this.playerId = '';
+
   this.initialize();
 };
 
-
 Player.prototype = {
   constructor: Player,
-  discard: function (tileId) {
 
-    var player = this;
-    console.log(player.playerID);
-    return db.Player.filter({gameId: player.gameID})
+  discard: function (tileId) {
+    return db.Player.get(this.playerId)
     .run()
-    .then(function (data) {
-      for(var i = 0; i < 3; i++) {
-        if(tileId === data[0].hand[i].tileId) {
-          data[0].hand.splice(i, 1);
+    .then(function (doc) {
+      for (var i = 0; i < 3; i++) {
+        if (tileId === doc.hand[i].tileId) {
+          doc.hand.splice(i, 1);
           break;
         }
       }
-      data.save()
-      .then(function (data) {
-        return data;
+      return doc.save()
+      .then(function (doc) {
+        return doc;
       })
       .catch(function (err) {
         console.error(err);
@@ -36,14 +34,42 @@ Player.prototype = {
     .catch(function (err) {
       console.error(err);
     });
-
   },
-  initialize: function () {
 
+  setRole: function(role) {
+    db.Player.get(this.playerId)
+    .update({role: role})
+    .then(function(doc) {
+      return doc;
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  },
+
+  changeTurn: function() {
+    db.Player.get(this.PlayerId)
+    .then(function(doc) {
+      doc.isTurn = !doc.isTurn;
+      return doc.save()
+      .then(function(doc) {
+        return doc;
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  },
+
+  initialize: function () {
     var player = this;
+
     return new db.Player({
-      gameId: player.gameID,
-      socketId: player.socketID,
+      gameId: player.gameId,
+      socketId: player.socketId,
       role: '',
       isTurn: false,
       hand: [],
@@ -51,8 +77,7 @@ Player.prototype = {
     })
     .save()
     .then(function (data) {
-      player.playerID = data.id;
-      //console.log(player.playerID);
+      player.playerId = data.id;
     })
     .catch(function (err) {
       console.error(err);
