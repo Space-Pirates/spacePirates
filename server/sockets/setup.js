cf6305cf60174021a67155a4c1227a09b08a9fe6
Jak Buttarazzi <JakRazzi@gmail.com>
2016-05-09 14:08:38 -0500

    (refactor) Abstract parseMove into a helpers file

--- server/sockets/setup.js
+++ server/sockets/setup.js
@@ -1,32 +1,7 @@
 var db = require('../db/db');
 var games = require('./../controllers/game-control').currentGames;
 var Player = require('./../game/player');
-
-var parseMove = function(x, y) {
-  if (x === 0 && y === 10) {
-    return 'discard';
-  } else if (y === 0) {
-    if (x >= 0 && x <= 2) {
-      return 'block1';
-    } else if (x >= 4 && x <= 6) {
-      return 'block2';
-    } else if (x >= 8 && x <= 10) {
-      return 'block3';
-    }
-  } else if (x >= 8 && x <= 10 && y === 10) {
-    return 'unblock';
-  } else if (x >= 0 && x <= 11 && y >= 1 && y <= 9) {
-    if (x === 9 && y === 3) {
-      return 'reveal1';
-    } else if (x === 9 && y === 5) {
-      return 'reveal2';
-    } else if (x === 9 && y === 7) {
-      return 'reveal3';
-    } else { // UPDATE
-      return 'update';
-    }
-  }
-}
+var utils = require('./socket-helpers');
 
 module.exports = function(app) {
 
@@ -59,7 +34,7 @@ module.exports = function(app) {
 
     // listen for moves
     socket.on('move', function(move) {
-      switch (parseMove(move.xEnd, move.yEnd)) {
+      switch (utils.parseMove(move.xEnd, move.yEnd)) {
         case 'discard':
           //
           break;
