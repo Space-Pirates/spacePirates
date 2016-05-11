a7845ae56884e1e097faf2e454c08136047aaa2c
Rob Bruhn <rdbruhn@gmail.com>
2016-05-11 01:40:45 -0500

    (chore) Remove socket update handler from socket.js

--- client/main-app/game/socket.js
+++ client/main-app/game/socket.js
@@ -17,14 +17,6 @@ function startSocketListeners() {
     createTile({x: 5, y: 10, tile: window.gameData.player.hand[2]});
   });
 
-  socket.on('update', function(data) {
-    // data has matrix, lastPlayed, x, y, tilesRemaining
-    window.gameData.board.matrix = data.matrix;
-    window.gameData.board.lastPlayed = data.lastPlayed;
-    window.gameData.deck.tilesRemaining = data.tilesRemaining;
-    createStaticTile({x: data.x, y: data.y, tile: data.lastPlayed});
-  });
-
   socket.on('deal', function(data) {
     window.gameData.player.hand = data.hand;
     window.gameData.board.lastPlayed = data.lastPlayed;
