b30e54bff5ed3e696a974bb56142f830566ca8d5
Rob Bruhn <rdbruhn@gmail.com>
2016-05-12 18:08:25 -0500

    (enhancement) Make game canvas transparent, remove load state img

--- client/phaser-app/load.js
+++ client/phaser-app/load.js
@@ -1,13 +1,8 @@
 var loadState = {
   preload: function() {
     // waiting for players sprite
-    game.load.image('nebula', 'phaser-app/assets/Nebula.png');
-
   },
   create: function(){
-    var background = game.add.sprite(0,0,'nebula');
-    background.height = 550;
-    background.width = 840;
 
     var loadingLabel = game.add.text(0, 0, 'Waiting for players...', {font:'30px Arial', fill:'#ffffff', boundsAlignH: "center", boundsAlignV: "middle"});
     loadingLabel.setTextBounds(0,0,840,550);
--- client/phaser-app/main.js
+++ client/phaser-app/main.js
@@ -1,5 +1,5 @@
 window.createGame = function(ele, scope, players, mapId, injector) {
-  window.game = new Phaser.Game(840, 550, Phaser.AUTO, 'gameCanvas');
+  window.game = new Phaser.Game(840, 550, Phaser.AUTO, 'gameCanvas', null, true);
   window.dragPosition = new Phaser.Point(0,0);
   game.state.add('load', loadState);
   game.state.add('game', gameState);
