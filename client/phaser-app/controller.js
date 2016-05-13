3a3f142f643662a4fca3ccc2d53f3b96e618a82b
Mike Khanoyan <mherkhanoyan@gmail.com>
2016-05-13 16:23:45 -0700

    (refactor) Update isValidMove, fix bug

--- client/phaser-app/controller.js
+++ client/phaser-app/controller.js
@@ -36,37 +36,42 @@ function onOut(sprite, pointer) {
 }
 
 function isValidMove(row, col, tile) {
+
+  // reject placement i
   if (gameData.board.matrix[row][col].tileId) {
-    console.log(0)
     return false;
   }
 
-  var leftTile = col > 0 && gameData.board.matrix[row][col - 1].right
-    ? gameData.board.matrix[row][col - 1].right : 0;
-  var rightTile = col < 11 && gameData.board.matrix[row][col + 1].left
-    ? gameData.board.matrix[row][col + 1].left : 0;
-  var topTile = col > 0 && gameData.board.matrix[row - 1][col].bottom
-    ? gameData.board.matrix[row - 1][col].bottom : 0;
-  var bottomTile = row < 8 && gameData.board.matrix[row + 1][col].top
-    ? gameData.board.matrix[row + 1][col].top : 0;
+  // get surrounding tile placement options
+  var surroundings = [
+    col > 0 && gameData.board.matrix[row][col - 1].right
+      ? gameData.board.matrix[row][col - 1].right : 0,
+    col < 11 && gameData.board.matrix[row][col + 1].left
+      ? gameData.board.matrix[row][col + 1].left : 0,
+    row > 0 && gameData.board.matrix[row - 1][col].bottom
+      ? gameData.board.matrix[row - 1][col].bottom : 0,
+    row < 8 && gameData.board.matrix[row + 1][col].top
+      ? gameData.board.matrix[row + 1][col].top : 0
+  ];
 
-  if (!(leftTile === 0 || tile.left === leftTile)) {
-    console.log(1)
+  var tileDirections = [tile.left, tile.right, tile.top, tile.bottom];
+
+  // reject tiles placed in empty space
+  if (!tileDirections.reduce((a, b) => a + b)) {
     return false;
   }
-  if (!(rightTile === 0 || tile.right === rightTile)) {
-    console.log(2)
-    return false;
+
+  // check each side and return false if not allowed to place
+  for (var i = 0; i < 4; i++) {
+    var adjacent = surroundings[i];
+    var side = tileDirections[i];
+    if (adjacent && side !== adjacent) {
+      return false;
+    }
   }
-  if (!(topTile === 0 || tile.top === topTile)) {
-    console.log(3)
-    return false;
-  }
-  if (!(bottomTile === 0 || tile.bottom === bottomTile)) {
-    console.log(4)
-    return false;
-  }
-  return gameData.player.isTurn && true;
+
+  // if legal move, check if is player's turn
+  return gameData.player.isTurn;
 }
 
 function onDragStart(sprite, pointer) {
@@ -79,11 +84,10 @@ function onDragStop(sprite, pointer) {
   var x = sprite.position.x/70;
   var y = sprite.position.y/50;
 
-  if (isValidMove(y-1, x, sprite.tileData)) {
+  if (isValidMove(y - 1, x, sprite.tileData)) {
     sprite.input.draggable = false;
     console.log(x,y);
     console.log(xInit, yInit);
-    //TODO: Write validation called checkPosition(sprite); ...will contain animation back to dragposition
     emitMove(xInit, yInit, x, y, sprite.tileData);
   } else {
     game.add.tween(sprite).to({x: dragPosition.x, y: dragPosition.y}, 500, 'Back.easeOut', true);
