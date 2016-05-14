a08488f946864b1ecbc414b4a60b438e05c25ddf
Jak Buttarazzi <JakRazzi@gmail.com>
2016-05-13 21:38:27 -0500

    (feat) Add parseMove to phaser controller

--- client/phaser-app/controller.js
+++ client/phaser-app/controller.js
@@ -93,3 +93,29 @@ function onDragStop(sprite, pointer) {
     game.add.tween(sprite).to({x: dragPosition.x, y: dragPosition.y}, 500, 'Back.easeOut', true);
   }
 }
+
+parseMove = function(x, y) {
+  if (x === 0 && y === 10) {
+    return 'discard';
+  } else if (y === 0) {
+    if (x >= 0 && x <= 2) {
+      return 'block1';
+    } else if (x >= 4 && x <= 6) {
+      return 'block2';
+    } else if (x >= 8 && x <= 10) {
+      return 'block3';
+    }
+  } else if (x >= 8 && x <= 10 && y === 10) {
+    return 'unblock';
+  } else if (x >= 0 && x <= 11 && y >= 1 && y <= 9) {
+    if (x === 9 && y === 3) {
+      return 'reveal1';
+    } else if (x === 9 && y === 5) {
+      return 'reveal2';
+    } else if (x === 9 && y === 7) {
+      return 'reveal3';
+    } else { // UPDATE
+      return 'update';
+    }
+  }
+}
