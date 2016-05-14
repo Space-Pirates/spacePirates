edb0b7a1b7751e4e602a74e40b02fde60d7a8d9a
Mike Khanoyan <mherkhanoyan@gmail.com>
2016-05-14 16:07:05 -0700

    (fix) Fix isEnded logic, add test

--- server/game/end_check.js
+++ server/game/end_check.js
@@ -11,30 +11,36 @@ module.exports.isEnded = function(matrix) {
   var endRows = [2, 4, 6];
 
   var ended = false;
+  var visited = {};
 
   (function findEnd(row, col) {
     var tile = matrix[row][col];
+
     if (col === endCol && endRows.indexOf(row) >= 0) {
       ended = true;
+      return;
     }
-
-    if (tile.tileId) {
-      if (tile.right === 1 && matrix[row][col + 1]
-        && matrix[row][col + 1].left === 1) {
-        findEnd(row, col + 1);
+    if (!visited[row + ',' + col]) {
+      visited[row + ',' + col] = 1;
+      if (tile.tileId) {
+        if (tile.right === 1 && matrix[row][col + 1]
+          && matrix[row][col + 1].left === 1) {
+          findEnd(row, col + 1);
+        }
+        if (tile.left === 1 && matrix[row][col - 1]
+          && matrix[row][col - 1].right === 1) {
+          findEnd(row, col - 1);
+        }
+        if (tile.top === 1 && matrix[row - 1]
+          && matrix[row - 1][col].bottom === 1) {
+          findEnd(row - 1, col);
+        }
+        if (tile.bottom === 1 && matrix[row + 1]
+          && matrix[row + 1][col].top === 1) {
+          findEnd(row + 1, col);
+        }
       }
-      if (tile.left === 1 && matrix[row][col - 1]
-        && matrix[row][col - 1].right === 1) {
-        findEnd(row, col - 1);
-      }
-      if (tile.top === 1 && matrix[row - 1]
-        && matrix[row - 1][col].bottom === 1) {
-        findEnd(row - 1, col);
-      }
-      if (tile.bottom === 1 && matrix[row + 1]
-        && matrix[row + 1][col].top === 1) {
-        findEnd(row + 1, col);
-      }
+      visited[row + ',' + col] = 0;
     }
   })(4, 1);
 
--- server/game/game-specs/end-check-specs.js
+++ server/game/game-specs/end-check-specs.js
@@ -8,7 +8,21 @@ describe('isEnded', function () {
 
   it('should return false for and incomplete route to planets', function() {
     var matrix = require('../board-matrix')();
+    matrix[4][1] = tiles[0]; // add start tile
     expect(check.isEnded(matrix)).to.be.equal(false);
+  });
+
+  it('should return false for and incomplete route to planets', function() {
+    var matrix = require('../board-matrix')();
+    matrix[4][1] = tiles[0]; // add start tile
+
+    // add intersection tiles all the way to the middle planet in straight line
+    for (var i = 2; i <= 8; i++) {
+      matrix[4][i] = tiles[0];
+    }
+
+    expect(check.isEnded(matrix)).to.be.equal(true);
+
   });
 
 });
