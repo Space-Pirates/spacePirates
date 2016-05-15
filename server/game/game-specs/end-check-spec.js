6b30a1380808a7c360ba0ac74cb22ac122a1f65b
Jak Buttarazzi <JakRazzi@gmail.com>
2016-05-14 19:29:16 -0500

    (feat) Update end game specs for new rules

--- server/game/game-specs/end-check-spec.js
+++ server/game/game-specs/end-check-spec.js
@@ -1,19 +1,33 @@
 var chai = require('chai');
-var check = require('../end_check');
+var check = require('../end-check');
 var tiles = require('../tile-dictionary');
 
 var expect = chai.expect;
 
 module.exports = function () {
 
+  it('should return false if remainingRoutes is 0', function() {
+    var matrix = require('../board-matrix')();
+    matrix[4][1] = tiles[0]; // add start tile
+    expect(check.isEnded(matrix, 0)).to.be.equal(false);
+  })
+
   it('should return false for and incomplete route to planets', function() {
     var matrix = require('../board-matrix')();
     matrix[4][1] = tiles[0]; // add start tile
-    expect(check.isEnded(matrix)).to.be.equal(false);
+    expect(check.isEnded(matrix, 31)).to.be.equal(false);
   });
 
-  it('should return ture for a complete route', function() {
+  it('should return true for a complete route', function() {
     var matrix = require('../board-matrix')();
+    // make true planet to go to
+    matrix[4][9] = {
+      truePlanet: true,
+      top: 1,
+      left: 1,
+      bottom: 1,
+      right: 1
+    };
     matrix[4][1] = tiles[0]; // add start tile
 
     // add intersection tiles all the way to the middle planet in straight line
@@ -21,7 +35,7 @@ module.exports = function () {
       matrix[4][i] = tiles[0];
     }
 
-    expect(check.isEnded(matrix)).to.be.equal(true);
+    expect(check.isEnded(matrix, 31)).to.be.equal(true);
 
   });
 
