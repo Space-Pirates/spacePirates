8b3d17c92c4c96cde97337012293441524b35ef5
nassir <nassirmz@gmail.com>
2016-05-17 15:47:21 -0700

    (style) Clean up code, delete console.logs

--- client/main-app/game/gameChat.js
+++ client/main-app/game/gameChat.js
@@ -13,31 +13,25 @@ angular.module('app.gameChat', [])
   $scope.videoStatus = true;
   $scope.mute = function () {
     $scope.audioStatus = window.ctrl.toggleAudio();
-    console.log($scope.audioStatus);
   };
 
   $scope.pause =  function() {
     $scope.videoStatus = window.ctrl.toggleVideo();
-    console.log($scope.videoStatus);
   };
 
   $rootScope.$on("$stateChangeStart", function (){
-    console.log(window.phone.number());
     window.ctrl.hangup();
   });
 }])
-.directive('showonhoverparent',function() {
+.directive('showonhover',function() {
   return {
     restrict: 'A',
     link : function(scope, element, attrs) {
-    console.log(element);
       element.parent().bind('mouseenter', function() {
-        console.log(element);
         element.css('display', 'inline');
-          // element.show();
       });
       element.parent().bind('mouseleave', function() {
-        element.css('display', 'none')
+        element.css('display', 'none');
       });
     }
   };
